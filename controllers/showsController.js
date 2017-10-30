61124e9ed0492112747a64ad72c1357ef8bd3286
Przemek <przkal1@gmail.com>
2017-10-30 20:26:32 +0100

    shows, design, sketches

--- controllers/showsController.js
+++ controllers/showsController.js
@@ -1,3 +1,17 @@
 angular.module("artistPortfolioApp").controller('showsController', function($scope, $route) {
     $scope.$parent.artSubmenuVisible = false;
+
+    var $gallery = $('#shows-gallery');
+    if ($gallery.length) {
+        $gallery.justifiedGallery({
+            border: 6,
+            captions: true,
+            margins: 20,
+            rowHeight: 350
+        }).on('jg.complete', function() {
+            $gallery.lightGallery({
+                thumbnail: true
+            });
+        });
+    };
 });
